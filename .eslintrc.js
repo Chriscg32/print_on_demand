6b946f3105d23cfd07f09725e607338e2f290213
chris gates <chrisgates32@gmail.com>
2025-03-25 17:28:28 +0200

    commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,10 +22,9 @@ module.exports = {
     node: true,
     es6: true,
     jest: true
-  },
+  }
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
+    'eslint:recommended',    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:@typescript-eslint/recommended',
