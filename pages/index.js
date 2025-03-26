6fc9aeca40b28aca5b08b11d8461c690defcd8df
chris gates <chrisgates32@gmail.com>
2025-03-26 20:18:36 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '2rem', maxWidth: '800px', margin: '0 auto' }}>
-      <h1 style={{ color: '#0070f3' }}>Print on Demand</h1>
-      <p>Welcome to the Print on Demand application!</p>
-      <div style={{ marginTop: '2rem', padding: '1rem', border: '1px solid #eaeaea', borderRadius: '5px' }}>
-        <h2>Ready for Deployment</h2>
-        <p>This application is now ready to be deployed to Vercel.</p>
-      </div>
-    </div>
-  );
-}
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Welcome to My Site</h1>
+  </div>
+);
+
+export default Home;
